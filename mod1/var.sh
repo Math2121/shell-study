ab8406ece59ebeb51de65cf79030cde3b9b14d45
Matheus da Silva de Paula <matheusdepaula@wisecut.video>
2024-07-05 15:27:45 -0300

    feat: iniciando estudo sobre shell

---
+++ mod1/var.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+echo "creating"
+
+NOME="Mth dfd"
+
+echo "$NOME"
+
+NUMERO_2=24
+
+echo "$NUMERO_2"
+
+NUMERO_3=25
+
+TOTAL=$((NUMERO_2 + NUMERO_3))
+
+
+echo "$TOTAL"
+
+SAIDA_CAT=$(cat /etc/passwd | grep math2121)
+
+echo "$SAIDA_CAT"
+
+# Var reservadas
+
+echo "Var reservada 1: $1"
+
+echo "Var reservada 2: $2"
+
+
+echo "Todos os var $*"
+
+QTD=$#
+
+echo "Quantidade de argumentos: $QTD"
+
+echo "Saída do ultimo comando $?"
+
+echo "PID $$"
+
+echo $0
+
+
